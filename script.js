b080c987e2ceebdff369cb27a8d18879931b2696
Daniel Zatorski <zatorskidaniel@gmail.com>
2025-09-13 12:54:08 +0200

    DOM wiring

---
+++ script.js
@@ -0,0 +1,19 @@
+//DOM WIRING
+
+const placeholderEl = document.getElementById("placeholder")
+
+//console.log(placeholderEl)
+
+const lengthEl = document.getElementById("result-1")
+const volumehEl = document.getElementById("result-2")
+const massEl = document.getElementById("result-3")
+
+const btnEl = document.getElementById("convert-btn")
+
+//console.log(btnEl)
+
+
+btnEl.addEventListener( "click", function(){
+
+
+})
