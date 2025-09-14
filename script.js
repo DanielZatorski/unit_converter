c2ce5e1b2f17008fc56031f05ba23218b8aa92d6
Daniel Zatorski <zatorskidaniel@gmail.com>
2025-09-14 10:25:08 +0200

    added second function for conversion to imperial and updated DOM manipulation of Event Listener

--- script.js
+++ script.js
@@ -15,17 +15,29 @@ const btnEl = document.getElementById("convert-btn")
 
 btnEl.addEventListener( "click", function(){
 
-    console.log(conversion(placeholderEl.value))
+    //console.log(conversion(placeholderEl.value))
 
-    let conversionResult = conversion(placeholderEl.value)
+    let conversionResultMetric = conversion_metric(placeholderEl.value)
+    let conversionResultImperial = conversion_imperial(placeholderEl.value)
 
     //console.log(conversionResult)
     //console.log(conversionResult.length)
 
-    lengthEl.textContent = Number(conversionResult[0]).toFixed(3);
-    volumehEl.textContent = Number(conversionResult[1]).toFixed(3);
-    massEl.textContent = Number(conversionResult[2]).toFixed(3)
-
+    lengthEl.textContent = `${placeholderEl.value} meters = 
+    ${Number(conversionResultMetric[0]).toFixed(3)} feet | 
+    ${placeholderEl.value} feet = 
+    ${Number(conversionResultImperial[0]).toFixed(3)} meters
+    `
+    volumehEl.textContent = `${placeholderEl.value} liters = 
+        ${Number(conversionResultMetric[1]).toFixed(3)} gallons | 
+        ${placeholderEl.value} gallons = 
+        ${Number(conversionResultImperial[1]).toFixed(3)} liters
+        `
+    massEl.textContent = `${placeholderEl.value} kilos = 
+        ${Number(conversionResultMetric[2]).toFixed(3)} pounds | 
+        ${placeholderEl.value} pounds = 
+        ${Number(conversionResultImperial[1]).toFixed(3)} kilos
+        `
 })
 
 //define conversion ratios
@@ -35,7 +47,7 @@ const liter = 0.264 //gallon
 const kilogram = 2.204 //pound
 
 //conversion function
-function conversion(input){
+function conversion_metric(input){
 
     
     const result_1 = input*meter
@@ -48,6 +60,18 @@ function conversion(input){
 
 }
 
+function conversion_imperial(input){
+
+    
+    const result_1 = input/meter
+    const result_2 = input/liter
+    const result_3 = input/kilogram
+    
+    results = [result_1,result_2,result_3]
+
+    return results
+
+}
 
 
 
